7abfc8330211fb744483ad69efd72f28fca296c0
Jesse Lawler <jedd.lawler@gmail.com>
2022-03-15 20:46:53 -0600

    built schema

---
+++ db/schema.sql
@@ -0,0 +1,33 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE department (
+  id int NOT NULL AUTO_INCREMENT,
+  name varchar(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+CREATE TABLE role (
+  id int NOT NULL AUTO_INCREMENT,
+  title varchar(30) NOT NULL,
+  salary decimal NOT NULL,
+  department_id int,
+  PRIMARY KEY (id),
+  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON UPDATE CASCADE ON DELETE
+  SET
+    NULL
+);
+CREATE TABLE employee (
+  id int NOT NULL AUTO_INCREMENT,
+  first_name varchar(30) NOT NULL,
+  last_name varchar(30) NOT NULL,
+  role_id int,
+  manager_id int,
+  PRIMARY KEY (id),
+  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON UPDATE CASCADE ON DELETE
+  SET
+    NULL,
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES (id) ON UPDATE CASCADE ON DELETE
+  SET
+    NULL
