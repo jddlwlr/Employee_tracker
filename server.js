c0192641506dae3c19d9324c5b7e81d474fa75da
Jesse Lawler <jedd.lawler@gmail.com>
2022-03-17 08:56:20 -0600

    added prompts, wireframed basic functions needed

--- server.js
+++ server.js
@@ -1,47 +1,102 @@
-// starter code
-const express = require("express");
-// Import and require mysql2
+// imports
 const mysql = require("mysql2");
+const inquirer = require("inquirer");
+const cTable = require("console.table");
 
-const PORT = process.env.PORT || 3001;
-const app = express();
+require("dotenv").config();
 
-// Express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-// Connect to database
-const db = mysql.createConnection(
-  {
-    host: "localhost",
-    // MySQL username,
-    user: "root",
-    // TODO: Add MySQL password here
-    password: "",
-    database: "movies_db",
-  },
-  console.log(`Connected to the movies_db database.`)
-);
-
-// Create a movie
-app.post("/api/new-movie", ({ body }, res) => {
-  const sql = `INSERT INTO movies (movie_name)
-    VALUES (?)`;
-  const params = [body.movie_name];
-
-  db.query(sql, params, (err, result) => {
-    if (err) {
-      res.status(400).json({ error: err.message });
-      return;
-    }
-    res.json({
-      message: "success",
-      data: body,
-    });
-  });
+// connection to database
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: process.env.MYSQL_PASSWORD,
+  database: "employee_db",
 });
 
-// Read all movies
+// Create a movie
+// app.post("/api/new-movie", ({ body }, res) => {
+//   const sql = `INSERT INTO movies (movie_name)
+//     VALUES (?)`;
+//   const params = [body.movie_name];
+
+//   db.query(sql, params, (err, result) => {
+//     if (err) {
+//       res.status(400).json({ error: err.message });
+//       return;
+//     }
+//     res.json({
+//       message: "success",
+//       data: body,
+//     });
+//   });
+// });
+
+const promptUser = () => {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "choices",
+        message: "What would you like to do?",
+        choices: [
+          "View all departments",
+          "View all roles",
+          "View all employees",
+          "Add a department",
+          "Add a role",
+          "Add an employee",
+          "Update an employee role",
+          "No Action",
+        ],
+      },
+    ])
+    .then((answers) => {
+      const { choices } = answers;
+
+      if (choices === "View all departments") {
+        logDepartments();
+      }
+
+      if (choices === "View all roles") {
+        logRoles();
+      }
+
+      if (choices === "View all employees") {
+        logEmployees();
+      }
+
+      if (choices === "Add a department") {
+        newDepartment();
+      }
+
+      if (choices === "Add a role") {
+        newRole();
+      }
+
+      if (choices === "Add an employee") {
+        newEmployee();
+      }
+
+      if (choices === "Update an employee role") {
+        updateEmployee();
+      }
+
+      if (choices === "No Action") {
+        connection.end();
+      }
+    });
+};
+
+logDepartments = () => {};
+logRoles = () => {};
+logEmployees = () => {};
+
+newDepartment = () => {};
+newRole = () => {};
+newEmployee = () => {};
+updateEmployee = () => {};
+
+// Modify this
 app.get("/api/movies", (req, res) => {
   const sql = `SELECT id, movie_name AS title FROM movies`;
 
@@ -57,29 +112,7 @@ app.get("/api/movies", (req, res) => {
   });
 });
 
-// Delete a movie
-app.delete("/api/movie/:id", (req, res) => {
-  const sql = `DELETE FROM movies WHERE id = ?`;
-  const params = [req.params.id];
-
-  db.query(sql, params, (err, result) => {
-    if (err) {
-      res.statusMessage(400).json({ error: res.message });
-    } else if (!result.affectedRows) {
-      res.json({
-        message: "Movie not found",
-      });
-    } else {
-      res.json({
-        message: "deleted",
-        changes: result.affectedRows,
-        id: req.params.id,
-      });
-    }
-  });
-});
-
-// Read list of all reviews and associated movie name using LEFT JOIN
+// And This
 app.get("/api/movie-reviews", (req, res) => {
   const sql = `SELECT movies.movie_name AS movie, reviews.review FROM reviews LEFT JOIN movies ON reviews.movie_id = movies.id ORDER BY movies.movie_name;`;
   db.query(sql, (err, rows) => {
@@ -94,27 +127,49 @@ app.get("/api/movie-reviews", (req, res) => {
   });
 });
 
-// BONUS: Update review name
-app.put("/api/review/:id", (req, res) => {
-  const sql = `UPDATE reviews SET review = ? WHERE id = ?`;
-  const params = [req.body.review, req.params.id];
+// Delete a movie
+// app.delete("/api/movie/:id", (req, res) => {
+//   const sql = `DELETE FROM movies WHERE id = ?`;
+//   const params = [req.params.id];
 
-  db.query(sql, params, (err, result) => {
-    if (err) {
-      res.status(400).json({ error: err.message });
-    } else if (!result.affectedRows) {
-      res.json({
-        message: "Movie not found",
-      });
-    } else {
-      res.json({
-        message: "success",
-        data: req.body,
-        changes: result.affectedRows,
-      });
-    }
-  });
-});
+//   db.query(sql, params, (err, result) => {
+//     if (err) {
+//       res.statusMessage(400).json({ error: res.message });
+//     } else if (!result.affectedRows) {
+//       res.json({
+//         message: "Movie not found",
+//       });
+//     } else {
+//       res.json({
+//         message: "deleted",
+//         changes: result.affectedRows,
+//         id: req.params.id,
+//       });
+//     }
+//   });
+// });
+
+// // BONUS: Update review name
+// app.put("/api/review/:id", (req, res) => {
+//   const sql = `UPDATE reviews SET review = ? WHERE id = ?`;
+//   const params = [req.body.review, req.params.id];
+
+//   db.query(sql, params, (err, result) => {
+//     if (err) {
+//       res.status(400).json({ error: err.message });
+//     } else if (!result.affectedRows) {
+//       res.json({
+//         message: "Movie not found",
+//       });
+//     } else {
+//       res.json({
+//         message: "success",
+//         data: req.body,
+//         changes: result.affectedRows,
+//       });
+//     }
+//   });
+// });
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
