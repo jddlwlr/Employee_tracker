19a36c654af8c44ffbbb829eee960502041a2d46
Jesse Lawler <jedd.lawler@gmail.com>
2022-03-17 12:04:37 -0600

    working on add functionality

--- server.js
+++ server.js
@@ -173,21 +173,22 @@ newRole = () => {
 
       db.query(roleSql, (err, data) => {
         if (err) throw err;
-
+        const dept = data.map(({ name, id }) => ({ name: name, value: id }));
         inquirer
           .prompt([
             {
               type: "list",
               name: "dept",
               message: "Deparment of Role?",
+              choices: dept,
             },
           ])
           .then((deptChoice) => {
             const sql = `INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)`;
-
+            const dept = deptChoice.dept;
+            params.push(dept);
             db.query(sql, params, (err, result) => {
               if (err) throw err;
-
               showRoles();
             });
           });
@@ -209,6 +210,58 @@ newEmployee = () => {
       }
     ])
       .then(answer => {
+        const params = [answer.fistName, answer.lastName]
+
+        const role = `SELECT role.id, role.title FROM role`;
+      
+        connection.promise().query(role, (err, data) => {
+          if (err) throw err; 
+          
+          const roles = data.map(({ id, title }) => ({ name: title, value: id }));
+    
+          inquirer.prompt([
+                {
+                  type: 'list',
+                  name: 'role',
+                  message: "What is the employee's role?",
+                  choices: roles
+                }
+              ])
+                .then(roleChoice => {
+                  const role = roleChoice.role;
+                  params.push(role);
+    
+                  const managerSql = `SELECT * FROM employee`;
+    
+                  connection.promise().query(managerSql, (err, data) => {
+                    if (err) throw err;
+    
+                    const managers = data.map(({ id, first_name, last_name }) => ({ name: first_name + " "+ last_name, value: id }));
+    
+                    inquirer.prompt([
+                      {
+                        type: 'list',
+                        name: 'manager',
+                        message: "Who is the employee's manager?",
+                        choices: managers
+                      }
+                    ])
+                      .then(managerChoice => {
+                        const manager = managerChoice.manager;
+                        params.push(manager);
+    
+                        const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id)
+                        VALUES (?, ?, ?, ?)`;
+    
+                        connection.query(sql, params, (err, result) => {
+                        if (err) throw err;
+                  });
+                });
+              });
+            });
+         });
+      });
+    };
 };
 
 // /new functions
